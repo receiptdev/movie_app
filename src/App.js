5ae9e211bbacf134378b6e20646a59bcd04f0156
receiptdev <receipt.defy@me.com>
2018-07-03 10:40:17 +0900

    Finish all

--- src/App.js
+++ src/App.js
@@ -1,51 +1,53 @@
-import React, {Component} from 'react';
-import './App.css';
-import Movie from './Movie';
-
-const movies = [
-    {
-        title: "Matrix",
-        poster: "https://upload.wikimedia.org/wikipedia/en/c/c1/The_Matrix_Poster.jpg"
-    },
-    {
-        title: "Full Metal Jacket",
-        poster: "https://upload.wikimedia.org/wikipedia/en/thumb/9/99/Full_Metal_Jacket_poster.jpg/220px-Full_Metal_Jacket_poster.jpg"
-    },
-    {
-        title: "Oldboy",
-        poster: "https://images-na.ssl-images-amazon.com/images/M/MV5BMTI3NTQyMzU5M15BMl5BanBnXkFtZTcwMTM2MjgyMQ@@._V1_UY1200_CR90,0,630,1200_AL_.jpg"
-    },
-    {
-        title: "Star wars",
-        poster: "https://i.pinimg.com/736x/a1/61/c7/a161c7034917936f8bd53ca65bc6f0d0--star-wars-stuff-art-illustrations.jpg"
-    }
-]
+import React, { Component } from "react";
+import "./App.css";
+import Movie from "./Movie";
 
 class App extends Component {
-    componentWillMount() {
-        console.log('will mount')
-    }
+    // Render: componentWillMount() -> render() -> componentDidMount()
+    // Update componentWillReceiveProps() -> shouldComponentUpdate() -> componentWillUpdate() -> render() -> componentDidUpdate()
+
+    state = {};
 
     componentDidMount() {
-        setTimeout(() => {
-            this.setState({
-                greeting: "Hello React"
-            })
-        }, 3000)
+        this._getMovies();
     }
 
-    state = {
-        greeting: "Hello"
-    }
+    _renderMovies = () => {
+        const movies = this.state.movies.map(movie => {
+            return (
+                <Movie
+                    title={movie.title_english}
+                    poster={movie.large_cover_image}
+                    key={movie.id}
+                    genres={movie.genres}
+                    synopsis={movie.synopsis}
+                />
+            );
+        });
+        return movies;
+    };
+
+    _getMovies = async () => {
+        const movies = await this._callApi();
+        this.setState({
+            movies
+        });
+    };
+
+    _callApi = () => {
+        return fetch(
+            "https://yts.am/api/v2/list_movies.json?sort_by=download_count"
+        )
+            .then(potato => potato.json())
+            .then(json => json.data.movies)
+            .catch(err => console.log(err));
+    };
 
     render() {
-        console.log('render')
+        const { movies } = this.state;
         return (
-            <div className="App">
-                {this.state.greeting}
-                {movies.map((movie, index) => {
-                    return <Movie title={movie.title} poster={movie.poster} key={index}/>
-                })}
+            <div className={movies ? "App" : "App--loading"}>
+                {movies ? this._renderMovies() : "Loading"}
             </div>
         );
     }
